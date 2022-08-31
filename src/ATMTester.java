298417ffdddc459ab9ef42a48801352ddcd5b923
Zach Rossen <zachary.rossen@gmail.com>
2022-08-31 11:12:43 -0700

    Tester
    Checks all functions work after closing unclosable account

--- src/ATMTester.java
+++ src/ATMTester.java
@@ -2,8 +2,18 @@
 public class ATMTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ATM bank = new ATM();
+		
+		bank.openAccount(42, 1099.99);
+		bank.closeAccount(42);
+		bank.checkBalance(42);
+		bank.depositMoney(42, 0.01);
+		bank.checkBalance(42);
+		bank.withdrawMoney(42, 2000.00);
+		bank.checkBalance(42);
+		bank.withdrawMoney(42, 100.00);
+		bank.checkBalance(42);
 	}
 
 }
